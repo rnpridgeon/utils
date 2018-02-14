92d47463b3f71845bdc419a3bdb0d333e6c02ceb
rnpridgeon <ryan.n.pridgeon@gmail.com>
2018-02-14 14:36:39 -0500

    add wg to test for more realistic example/test

--- workerpool/workerpool_test.go
+++ workerpool/workerpool_test.go
@@ -2,28 +2,29 @@ package workerpool
 
 import(
 	"testing"
+	//"log"
+	"sync"
 	"time"
-	"fmt"
-	"sync/atomic"
 )
 
 const cycles = 1000
 
 func TestCleaner(t *testing.T) {
 	w := NewWorkerManager(10)
-	var inFlight int64 = 0
+	wg := sync.WaitGroup{}
+	//done := make(chan struct{})
 	for i := 0; i <= cycles; i++ {
+		wg.Add(1)
 		taskCh := <-w.Ready
-			atomic.AddInt64(&inFlight, 1)
 			taskCh.TaskChanel<-func(){
-						time.Sleep(1 * time.Second)
-						atomic.AddInt64(&inFlight, -1)
+				wg.Done()
 				}
 	}
-	fmt.Println(inFlight)
-	time.Sleep(cleanerCheckInterval + 1 * time.Second)
+
+	wg.Wait()
+	time.Sleep(cleanerCheckInterval)
+
 	// There should always be 1 worker in the chamber
-	fmt.Println(inFlight)
 	if w.capacity != 9 {
 		t.Error("Wrokerpool capacity failed to grow after ")
 	}
